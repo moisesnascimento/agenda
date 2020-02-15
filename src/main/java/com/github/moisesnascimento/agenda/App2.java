ccf2659dde6d7a07b9af8c72cc6c99262a614654
Moises Nascimento <moises.smith759@gmail.com>
2020-02-15 18:55:13 -0300

    teste

--- src/main/java/com/github/moisesnascimento/agenda/App2.java
+++ src/main/java/com/github/moisesnascimento/agenda/App2.java
@@ -9,7 +9,7 @@ public class App2
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+        System.out.println( "Moises" );
         System.out.println( "Hello World!" );
     }
 }
